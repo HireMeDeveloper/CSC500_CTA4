72170593abe5740137b04a07816a93ca2361cbc4
hiremedev <hiremedev@penguin>
2025-05-31 16:15:55 -0700

    Updating for final project

--- PortfolioMilestone1.py
+++ PortfolioMilestone1.py
@@ -88,6 +88,7 @@ def get_item(itemNum: int) -> ItemToPurchase:
     print("Item " + str(itemNum) + ": ")
     item = ItemToPurchase()
     item.item_name = input("Enter the item name: ")
+    item.item_description = input("Enter the item description: ")
     item.item_price = float(input("Enter the item price: "))
     item.item_quantity = int(input("Enter the item quantity: ")) 
     return item 
@@ -123,16 +124,28 @@ def print_menu(shoppingCart: ShoppingCart):
                 print()
 
 def add(cart: ShoppingCart) -> bool:
-    print("Adding Item")
-    print()
+    print("ADD ITEM TO CART")
     cart.add_item(get_item(1))
+    print()
     return True
 def remove(cart: ShoppingCart) -> bool:
-    print("Removing Item")
+    print("REMOVE ITEM FROM CART")
+    itemName = input("Enter name of item to remove: ")
+    cart.remove_item(itemName)
     print()
     return True
 def change_quantity(cart: ShoppingCart) -> bool:
-    print("Changing quantity")
+    print("CHANGE ITEM QUANTITY")
+    itemName = input('Enter the item name:')
+    quantity = int(input('Enter the new quantity: '))
+    item = cart.find_item_by_name(itemName)
+    if (item == None):
+        print("Failed to find item.")
+    else:
+        modify = ItemToPurchase()
+        modify.item_name = itemName
+        modify.item_quantity = quantity
+        cart.modify_item(modify)
     print()
     return True
 def output_descriptions(cart: ShoppingCart) -> bool:
@@ -149,4 +162,11 @@ def quit(cart: ShoppingCart) -> bool:
     print("Goodbye")
     print()
     return False
-print_menu(ShoppingCart("Jeffrey Lamb", "January 21, 2025"))
+
+name = input("Enter customer's name:")
+date = input("Enter today's date:")
+shoppingCart = ShoppingCart(name, date)
+print("Customer name: " + name)
+print("Today's date: " + date)
+
+print_menu(shoppingCart)
